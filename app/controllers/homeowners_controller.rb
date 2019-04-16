e0a091c6d9a1895d9fe36190971a765ec62baeb9
github username <github email address>
2019-04-16 15:33:26 -0600

    A homeowner is able to create themselves with full attributes

--- app/controllers/homeowners_controller.rb
+++ app/controllers/homeowners_controller.rb
@@ -1,2 +1,46 @@
 class HomeownersController < ApplicationController
+  
+  def index
+    @homeowners = Homeowner.all
+  end
+
+  def new
+    @homeowner = Homeowner.new
+  end
+
+  def create
+    @homeowner = Homeowner.create(homeowner_params)
+    if (@homeowner.errors.any?)
+      render :new
+    else
+    redirect_to homeowners_path
+    end
+  end
+
+  def show
+    @homeowner = Homeowner.find(params[:id])
+  end
+
+  def edit
+    @homeowner = Homeowner.find(params[:id])
+  end
+
+  def update
+    @homeowner = Homeowner.update(homeowner_params)
+      redirect_to homeowners_path
+  end
+
+  def destroy
+    @homeowner = Homeowner.find(params[:id])
+    @homeowner.delete
+
+    redirect_to homeowners_path
+  end
+
+  private
+
+  def homeowner_params
+    params.require(:homeowner).permit(:first_name, :last_name, :street_name,
+                                      :city, :state, :zipcode, :email)
+  end
 end
