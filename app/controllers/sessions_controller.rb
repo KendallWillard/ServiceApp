29631679824ec59e3d5c7593eb2c7f4730fbe7ad
github username <github email address>
2019-04-16 17:15:33 -0600

    added logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,13 @@ class SessionsController < ApplicationController
   def create
     # byebug
     @homeowner = Homeowner.find_by(username: params[:user][:name])
-    head(:forbidden) unless @homeowner.authenticate(params[:user][:password])
+    return head(:forbidden) unless @homeowner.authenticate(params[:user][:password])
     session[:homeowner_id] = @homeowner.id
-    render 'homeowners/show'
+    redirect_to homeowner_path(@homeowner)
+  end
+
+  def destroy
+    session.delete :username
+    redirect_to '/login'
   end
 end
