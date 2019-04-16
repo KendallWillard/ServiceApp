77a62cd701d4539461189f59fd2f8eeecfbd2282
github username <github email address>
2019-04-16 11:01:46 -0600

    created homeowners, reviews, and postings model, along with associated relationships

--- db/migrate/20190416160533_create_homeowners.rb
+++ db/migrate/20190416160533_create_homeowners.rb
@@ -1,7 +1,14 @@
 class CreateHomeowners < ActiveRecord::Migration[5.2]
   def change
     create_table :homeowners do |t|
-
+      t.text :first_name
+      t.text :last_name
+      t.text :street_name
+      t.text :city
+      t.text :state
+      t.integer :zipcode
+      t.text :email
+      t.references :postings
       t.timestamps
     end
   end
