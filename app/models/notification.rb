ae67abaddbf3586be17ea51b781899e8e716294d
github username <github email address>
2019-04-16 17:15:22 -0600

    add service provider views

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
-  belongs_to :appointment, :service, :posting 
+  belongs_to :appointment
+  belongs_to :service
+  belongs_to :posting 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 
-Service.destroy_all
+# Service.destroy_all
 
 framing = Service.create(name: "Framing", description: "Rough-in framing; stick-built, steel stud, block, or post and beam.")
 concrete = Service.create(name: "Concrete", description: "Structural concrete.")
@@ -31,4 +31,4 @@ fixtures_and_finishes = Service.create(name: "Fixtures and Finishes", descriptio
 
 # jane_doe = Homeowner.create(name: "Jane Doe", address: "1247 N Cantilever Ln Loveland, CO 80537", phone: 7201148945, email: "jane_do3@email.com")
 
-jack_of_all_trades = Service_Provider.create(type: "Freelancer", name: "Jack of All Trades", street_name: "411 Main St", city: "Loveland", state: "CO", zipcode: 80537, service_area: "50 mile radius", years_in_service: 8)
+ jack_of_all_trades = ServiceProvider.create(service_provider_type: "Freelancer", name: "Jack of All Trades", street_name: "411 Main St", city: "Loveland", state: "CO", zipcode: 80537, service_area: "50 mile radius", years_in_service: 8)
