71489210069b09de7844c8233d66a237ea1d4648
github username <github email address>
2019-04-19 09:37:51 -0600

    debug deletion of appointment upon posting creation

---
+++ app/assets/images/image_list.rb
@@ -0,0 +1,21 @@
+
+photo_urls = {
+"Framing" => https://senaterace2012.com/wp-content/uploads/timber-framed-house-fenzel-pine-pinus-fenzelania_135638.jpg,
+"Concrete" => https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjBlb_pk9vhAhUHR6wKHcWrC9EQjRx6BAgBEAU&url=https%3A%2F%2Fwww.thespruce.com%2Ftypes-of-house-foundations-1821308&psig=AOvVaw1POYZKUOhgbOi0Zb4ST3x_&ust=1555728705876699,
+"Plumbing" => https://joesplumbingco.com/wp-content/uploads/2017/03/plumbing-tools.jpg,
+"Landscape" => https://i.pinimg.com/originals/10/ba/15/10ba1504b763821d2ec94b8601d86e3e.jpg,
+"Electrical" => https://st3.depositphotos.com/3261317/18924/i/1600/depositphotos_189248062-stock-photo-top-view-of-working-tools.jpg,
+"HVAC" => https://hvacinds.com/wp-content/uploads/2017/12/grove-waltham-hvac-residential-project-1.jpg,
+"Painting" => https://www.avrpainting.com/wp-content/uploads/2018/01/Painting-Services-miami-fl-Home-Service-residential-painting.png,
+"Sheetrock" => https://firedawgsjunkremoval.com/images/Drywall-Sheetrock-Cleanup-Indianapolis-1200x900.jpg,
+"Flooring" => https://www.phenixflooring.com/files/homepageslides/PhenixSliders-PPDesignSolutions4new.jpg,
+"Roofing" => https://d3fgmcoixbear.cloudfront.net/s3fs-public/colors/308747-LM-MaxDefWeatheredWood.JPG,
+"Insulation" => https://metalbuildinghomes.org/wp-content/uploads/2016/02/insulation-options-for-steel-buildings.jpg,
+"Cabinetry" => https://www.durasupreme.com/sites/www.durasupreme.com/files/RaisedPanelDoors_2018-black.jpg,
+"Doors" => https://bulgara-ny.com/img/door/wooden-door-types/27_various-types-of-residential-doors_doors-ppt.jpg,
+"Windows" => http://www.davidmaudlin.com/ArchiCAD/DigitalArchitecture/ResLib/ResLib08W/ResLib08wGrilleTypes.gif,
+"Masonry" => https://i.pinimg.com/736x/4b/46/01/4b4601b41b3622c5bebcf77a324b8456--architecture-details-architecture-drawings.jpg,
+"Stucco" => https://thestuccoguy.com/wp-content/uploads/2013/07/Stucco-Textures-And-Finishes.jpg,
+"Siding" => https://www.lavalleys.net/MainSite/Store1/_CachedContent/_ECommerce/StoreProductCategories/1040.jpg,
+"Fixtures and Finishes" => 
+}
